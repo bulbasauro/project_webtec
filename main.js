6f9927cfa8cf74a9b3cb9ba60218842e1781422b
AnaPriscila <ana_priscila_oliveira@hotmail.com>
2019-09-16 19:24:42 -0300

    separação javascript de html
    injeção e remoção de atributo

---
+++ main.js
@@ -0,0 +1,5 @@
+var tit = document.getElementById('titulo');
+tit.innerHTML='Teste Imagem com HTML'
+var bulba = document.getElementById('imagem');
+bulba.src='file:///C:/Users/ana_p/OneDrive/Imagens/Saved%20Pictures/05rpg.jpg';
+bulba.removeAttribute('alt');
