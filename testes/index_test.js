2bf93bfbbc3e4153d66b71a1afd563ea0661cc36
AnaPriscila <ana_priscila_oliveira@hotmail.com>
2019-10-14 19:36:38 -0300

    Alteração para Ambiente da Feia
    - Altera para endereço do disco local
    - Adiciona imagem salva na pasta output para o projeto de teste

--- testes/index_test.js
+++ testes/index_test.js
@@ -1,7 +1,10 @@
+const {I} = inject();
+const baseUrl = 'file:///C:/Users/ana_p/Ana_Projetos/project_webtec';
 
 Feature('default');
 
 Scenario('test something', (I) => {
-    I.amOnPage('file:///D:/GitHub/project_webtec/index.html');
+    I.amOnPage(`${baseUrl}/index.html`);
     I.see('Teste Imagem com HTML');
+    I.saveScreenshot('imagem.png');
 });
