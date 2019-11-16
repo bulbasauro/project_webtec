84b6fbe7bb585ab67f1d36849d101510e5fe285a
AnaPriscila <ana_priscila_oliveira@hotmail.com>
2019-11-15 22:38:22 -0300

    Adiciona arquivo style.css
    Adiciona arquivo .css para organizar estilo do site

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 const body = document.body;
 
 var tit = document.getElementById('titulo');
-tit.innerHTML='Teste Imagem com HTML'
+    tit.innerHTML='Bulbasauro é um pokemon do tipo planta!';
+
 var bulba = document.getElementById('imagem');
+    bulba.src='./imagens/bulbasaurs_01.jpg';
+    bulba.removeAttribute('alt');
+    
 //bulba.src='file:///C:/Users/ana_p/OneDrive/Imagens/Saved%20Pictures/05rpg.jpg';
-bulba.src='./imagens/bulbasaurs_01.jpg';
-bulba.removeAttribute('alt');
+
